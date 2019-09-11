8a89bbad5cda277d57bffcda7854f98fd9dda88f
hadxu <hadxu123@gmail.com>
2019-09-11 23:00:32 +0800

    temporal_attention

--- src/models.py
+++ src/models.py
@@ -2,31 +2,42 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from loader import VQADataset
+from torch.nn import functional as F
 
 
-class TimeDistributed(nn.Module):
-    def __init__(self, module, batch_first=False):
-        super(TimeDistributed, self).__init__()
-        self.module = module
-        self.batch_first = batch_first
+def distance(q, v, dist, dim=2):
+    if dist == 'dice':
+        return q * v / (torch.sum(q ** 2, dim=dim, keepdim=True) + torch.sum(v ** 2, dim=dim, keepdim=True))
 
-    def forward(self, x):
 
-        if len(x.size()) <= 2:
-            return self.module(x)
+def temporal_attention(q, v, use_conv=False):
+    def get_atten_w(q_list, v_encode, fca):
+        w_list = []
+        for q in q_list:
+            merged = distance(q, v_encode, 'dice')
 
-        x_reshape = x.contiguous().view(-1, x.size(-1))
+            w = fca(merged)
+            w = w.view(-1)
+            w = F.softmax(w)
+            w_list.append(w)
 
-        y = self.module(x_reshape)
+        w = torch.mean(torch.cat(w_list))
+        w = w.view(-1, 1, 1)
+        return w
 
-        print(y.size())
+    q = map(lambda x: torch.mean(x, dim=1), q)
 
-        if self.batch_first:
-            y = y.contiguous().view(x.size(0), -1, y.size(-1))
-        else:
-            y = y.view(-1, x.size(1), y.size(-1))
+    q = map(nn.Linear(300, 256), q)
+    q = map(lambda x: torch.reshape(x, (-1, 1, 256)), q)
 
-        return y
+    fca = nn.Linear(256, 1)
+
+    v_encode = torch.mean(v, dim=2)
+    v_encode = nn.Linear(2048, 256)(v_encode)
+
+    w = get_atten_w(q, v_encode, fca)
+
+    return v * w
 
 
 class VQANet(nn.Module):
@@ -43,14 +54,11 @@ class VQANet(nn.Module):
             nn.Dropout(0.05)
         )
 
-        self.time_distribute1 = nn.Sequential(
-            TimeDistributed(self.emb),
-        )
-
-    def forward(self, video, ques, attr):
+    def forward(self, video, ques, attr, prior):
         print(video.size())
         print(ques.size())
         print(attr.size())
+        print(prior.size())
 
         print('============')
 
@@ -62,7 +70,8 @@ class VQANet(nn.Module):
         v = self.video_dp1(video)
         attr = self.emb(attr)
 
-        print(q1.size())
+        v = temporal_attention([q1, q2, q3, q4, q5], v)
+
         print(v.size())
         print(attr.size())
 
@@ -76,4 +85,4 @@ if __name__ == '__main__':
     net = VQANet()
 
     for video, attr, ques, prior, y, ans in loader:
-        pred = net(video, ques, attr)
+        pred = net(video, ques, attr, prior)
